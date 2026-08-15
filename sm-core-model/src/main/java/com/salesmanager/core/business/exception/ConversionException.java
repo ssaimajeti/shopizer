eb23c5d0e7cce3a1926fa238abef68b0a9b3380a
Satya Sai <satyasai.majeti@xebia.com>
2026-08-15 12:42:02 +0530

    [JTT-2349] Add sm-core-model/src/main/java/com/salesmanager/core/business/exception/ConversionException.java

--- sm-core-model/src/main/java/com/salesmanager/core/business/exception/ConversionException.java
+++ sm-core-model/src/main/java/com/salesmanager/core/business/exception/ConversionException.java
@@ -4,28 +4,24 @@
 package com.salesmanager.core.business.exception;
 
 /**
- * @author Umesh A
- *
+ * Represents an exception that occurs during the conversion process.
+ * A detailed message and cause can be provided to describe the specific conversion problem.
+ * This exception extends from Java's base Exception class.
+ * It is typically used to wrap any lower-level exceptions encountered during conversion.
+ * Author: Umesh A
  */
-public class ConversionException extends Exception
-{
+public class ConversionException extends Exception {
   private static final long serialVersionUID = 687400310032876603L;
   
-  public ConversionException(final String msg, final Throwable cause)
-  {
+  public ConversionException(final String msg, final Throwable cause) {
       super(msg, cause);
   }
 
-  public ConversionException(final String msg)
-  {
+  public ConversionException(final String msg) {
       super(msg);
   }
   
-  public ConversionException(Throwable t)
-  {
+  public ConversionException(Throwable t) {
       super(t);
   }
-  
-  
-
 }
