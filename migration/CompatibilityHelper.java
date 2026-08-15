89430b92c9cfc5f77f64270803e25c083ee0e874
Satya Sai <satyasai.majeti@xebia.com>
2026-08-15 23:12:53 +0530

    [JTT-2383] Add migration/CompatibilityHelper.java

--- migration/CompatibilityHelper.java
+++ migration/CompatibilityHelper.java
@@ -5,24 +5,23 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-// Renaming javax.* to jakarta.* as per Spring Boot 3.x transition
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Index;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
-import jakarta.persistence.TableGenerator;
-import jakarta.persistence.Transient;
-import jakarta.persistence.UniqueConstraint;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Pattern;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.TableGenerator;
+import javax.persistence.Transient;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 
 import org.springframework.web.multipart.MultipartFile;
 
@@ -155,5 +154,3 @@ public class ProductOptionValue extends SalesManagerEntity<Long, ProductOptionVa
 		return code;
 	}
 }
-
-// Following logical updates for package renaming and deprecated API replacements can be applied to appropriate files as shown in this example. Make sure the version constraint is respected throughout CI/CD configuration and supporting files like pom.xml, Dockerfiles, etc.
