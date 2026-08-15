c3d304fc14c5ad8c7c650beca368e225a9be0134
Satya Sai <satyasai.majeti@xebia.com>
2026-08-15 23:05:55 +0530

    [JTT-2383] Add sm-core-model/src/main/java/com/salesmanager/core/business/exception/ConversionException.java

--- sm-core-model/src/main/java/com/salesmanager/core/business/exception/ConversionException.java
+++ sm-core-model/src/main/java/com/salesmanager/core/business/exception/ConversionException.java
@@ -4,19 +4,8 @@
 package com.salesmanager.core.business.exception;
 
 /**
- * Custom exception class for conversion-related errors.
+ * Custom exception for conversion errors.
  * 
- * Represents an error during the conversion process.
- * This class extends the base Exception class.
- * It provides multiple constructors to create instances
- * with a custom message and an optional cause.
- * 
- * @version 1.0
- * @since 2.0
- * 
- * @see java.lang.Exception
- * 
- * Author: Umesh A
  */
 public class ConversionException extends Exception
 {
@@ -36,4 +25,5 @@ public class ConversionException extends Exception
   {
       super(t);
   }
+
 }
