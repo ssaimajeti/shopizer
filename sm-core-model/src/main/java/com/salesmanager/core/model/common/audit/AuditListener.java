af6789b125ba625029c5beff390a0d537466df00
Satya Sai <satyasai.majeti@xebia.com>
2026-08-16 07:27:18 +0530

    [JTT-2383] Add sm-core-model/src/main/java/com/salesmanager/core/model/common/audit/AuditListener.java

--- sm-core-model/src/main/java/com/salesmanager/core/model/common/audit/AuditListener.java
+++ sm-core-model/src/main/java/com/salesmanager/core/model/common/audit/AuditListener.java
@@ -1,8 +1,8 @@
 package com.salesmanager.core.model.common.audit;
 
 import java.util.Date;
-import javax.persistence.PrePersist;
-import javax.persistence.PreUpdate;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 
 public class AuditListener {
 
