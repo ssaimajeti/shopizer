78bd84100f696bb7f5f3dafd4a405ba98e9da39a
Satya Sai <satyasai.majeti@xebia.com>
2026-08-16 07:26:46 +0530

    [JTT-2383] Add sm-core-model/src/main/java/com/salesmanager/core/model/catalog/product/variation/ProductVariation.java

--- sm-core-model/src/main/java/com/salesmanager/core/model/catalog/product/variation/ProductVariation.java
+++ sm-core-model/src/main/java/com/salesmanager/core/model/catalog/product/variation/ProductVariation.java
@@ -1,19 +1,19 @@
 package com.salesmanager.core.model.catalog.product.variation;
 
-import javax.persistence.Column;
-import javax.persistence.Embedded;
-import javax.persistence.Entity;
-import javax.persistence.EntityListeners;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-import javax.persistence.TableGenerator;
-import javax.persistence.UniqueConstraint;
-import javax.validation.constraints.NotEmpty;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.TableGenerator;
+import jakarta.persistence.UniqueConstraint;
+import jakarta.validation.constraints.NotEmpty;
 
 import com.salesmanager.core.model.catalog.product.attribute.Optionable;
 import com.salesmanager.core.model.catalog.product.attribute.ProductOption;
