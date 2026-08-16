65f3820ea3646fd1591ed0043aa06a4fcc908048
Satya Sai <satyasai.majeti@xebia.com>
2026-08-16 07:27:06 +0530

    [JTT-2383] Add sm-core-model/src/main/java/com/salesmanager/core/model/order/orderproduct/OrderProductDownload.java

--- sm-core-model/src/main/java/com/salesmanager/core/model/order/orderproduct/OrderProductDownload.java
+++ sm-core-model/src/main/java/com/salesmanager/core/model/order/orderproduct/OrderProductDownload.java
@@ -2,15 +2,15 @@ package com.salesmanager.core.model.order.orderproduct;
 
 import java.io.Serializable;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-import javax.persistence.TableGenerator;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.TableGenerator;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.salesmanager.core.constants.SchemaConstant;
