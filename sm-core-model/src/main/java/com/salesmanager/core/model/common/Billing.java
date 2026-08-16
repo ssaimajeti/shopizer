6e92b2b67a24c9539d9577f7651e38205c5e7bfc
Satya Sai <satyasai.majeti@xebia.com>
2026-08-16 07:28:38 +0530

    [JTT-2383] Add sm-core-model/src/main/java/com/salesmanager/core/model/common/Billing.java

--- sm-core-model/src/main/java/com/salesmanager/core/model/common/Billing.java
+++ sm-core-model/src/main/java/com/salesmanager/core/model/common/Billing.java
@@ -1,12 +1,12 @@
 package com.salesmanager.core.model.common;
 
-import javax.persistence.Column;
-import javax.persistence.Embeddable;
-import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
-import javax.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotEmpty;
 
 import com.salesmanager.core.model.reference.country.Country;
 import com.salesmanager.core.model.reference.zone.Zone;
