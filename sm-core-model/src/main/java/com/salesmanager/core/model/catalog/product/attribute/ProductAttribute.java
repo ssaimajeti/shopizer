16a318d0338865e6a5e57dd124317db7136b45a8
Satya Sai <satyasai.majeti@xebia.com>
2026-08-25 12:54:38 +0530

    [JTT-2496] Add sm-core-model/src/main/java/com/salesmanager/core/model/catalog/product/attribute/ProductAttribute.java

--- sm-core-model/src/main/java/com/salesmanager/core/model/catalog/product/attribute/ProductAttribute.java
+++ sm-core-model/src/main/java/com/salesmanager/core/model/catalog/product/attribute/ProductAttribute.java
@@ -2,19 +2,19 @@ package com.salesmanager.core.model.catalog.product.attribute;
 
 import java.math.BigDecimal;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-import javax.persistence.TableGenerator;
-import javax.persistence.Transient;
-import javax.persistence.UniqueConstraint;
-import javax.persistence.Index;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.TableGenerator;
+import jakarta.persistence.Transient;
+import jakarta.persistence.UniqueConstraint;
+import jakarta.persistence.Index;
 
 import com.salesmanager.core.model.catalog.product.Product;
 import com.salesmanager.core.model.generic.SalesManagerEntity;
