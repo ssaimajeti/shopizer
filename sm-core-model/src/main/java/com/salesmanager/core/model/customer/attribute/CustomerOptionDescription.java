713a5cf5e7663ca5b4c20744b1637d383923e7f2
Satya Sai <satyasai.majeti@xebia.com>
2026-08-25 12:54:08 +0530

    [JTT-2496] Add sm-core-model/src/main/java/com/salesmanager/core/model/customer/attribute/CustomerOptionDescription.java

--- sm-core-model/src/main/java/com/salesmanager/core/model/customer/attribute/CustomerOptionDescription.java
+++ sm-core-model/src/main/java/com/salesmanager/core/model/customer/attribute/CustomerOptionDescription.java
@@ -1,13 +1,13 @@
 package com.salesmanager.core.model.customer.attribute;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.SequenceGenerator;
-import javax.persistence.Table;
-import javax.persistence.TableGenerator;
-import javax.persistence.UniqueConstraint;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import jakarta.persistence.TableGenerator;
+import jakarta.persistence.UniqueConstraint;
 
 import org.hibernate.annotations.Type;
 
