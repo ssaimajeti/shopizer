dc616bad1e7d5a7e729e7da68bc0d10863a26755
Satya Sai <satyasai.majeti@xebia.com>
2026-08-25 12:54:07 +0530

    [JTT-2496] Add sm-core-model/src/main/java/com/salesmanager/core/model/reference/geozone/GeoZone.java

--- sm-core-model/src/main/java/com/salesmanager/core/model/reference/geozone/GeoZone.java
+++ sm-core-model/src/main/java/com/salesmanager/core/model/reference/geozone/GeoZone.java
@@ -3,15 +3,15 @@ package com.salesmanager.core.model.reference.geozone;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-import javax.persistence.TableGenerator;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.persistence.TableGenerator;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.salesmanager.core.constants.SchemaConstant;
