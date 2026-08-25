bc9bc1da44909b0266dc136cedc82a56678d6482
Satya Sai <satyasai.majeti@xebia.com>
2026-08-25 12:55:36 +0530

    [JTT-2496] Add sm-core-model/src/main/java/com/salesmanager/core/model/shipping/Quote.java

--- sm-core-model/src/main/java/com/salesmanager/core/model/shipping/Quote.java
+++ sm-core-model/src/main/java/com/salesmanager/core/model/shipping/Quote.java
@@ -3,16 +3,16 @@ package com.salesmanager.core.model.shipping;
 import java.math.BigDecimal;
 import java.util.Date;
 
-import javax.persistence.Column;
-import javax.persistence.Embedded;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.TableGenerator;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.TableGenerator;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 
 import com.salesmanager.core.constants.SchemaConstant;
 import com.salesmanager.core.model.common.Delivery;
